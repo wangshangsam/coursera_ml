f4d60e8883f25585af547631efa6cb88f674b1bd
Shang Wang <samshang.wang@mail.utoronto.ca>
2016-10-17 02:04:04 -0700

    Finish part1, 2 and 3 of ex7.

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -21,10 +21,23 @@ idx = zeros(size(X,1), 1);
 % Note: You can use a for-loop over the examples to compute this.
 %
 
-
-
-
-
+for i = 1:size(X, 1)
+    x = X(i, :);
+    Expand_x = ones(K, 1) * x;  % Expand_x is a matrix that has k rows, each
+                                % row is the same row vector as x.
+    Dif = Expand_x - centroids; % Dif is a matrix that has k rows, each row
+                                % is the vector substraction between x and
+                                % a centroid.
+    [d, idx(i)] = min(diag(Dif * Dif'));
+                                % Dif * Dif' gives a matrix that the element
+                                % on the i_th row and j_th column is the dot
+                                % product between the i_th row vector in Dif
+                                % and the j_th row vector in Dif; therefore,
+                                % the diagonal of this matrix is the dot product
+                                % of each row vector with itself, i.e. the
+                                % norm^2 of each row vector. Our job then
+                                % becomes finding the minimum in this diagonal.
+end
 
 
 % =============================================================
